60f946e0cde40190cbe3e739b02a8a56c4714007
Altava <fabian.bigbrain@gmail.com>
2022-10-17 19:17:49 +0200

    parser error fixed

--- pddl/lab_run_configurations/parser_tfd_anytime.py
+++ pddl/lab_run_configurations/parser_tfd_anytime.py
@@ -7,7 +7,7 @@ from lab.parser import Parser
 
 def parse_times_over_time(content, props):
     matches = re.findall(r"Found plan after (\d+) sec.", content)
-    props["times_over_time"] = [(float(t), float(s)) for t, s in matches]
+    props["times_over_time"] = [float(s) for s in matches]
 
 def parse_plan_length_over_time(content, props):
     matches = re.findall(r"Plan length: (\d+) step\(s\).", content)
@@ -15,7 +15,7 @@ def parse_plan_length_over_time(content, props):
 
 def parse_plan_cost_over_time(content, props):
     matches = re.findall(r"Makespan   : (.+)", content)
-    props["plan_cost_over_time"] = [int(s) for s in matches]
+    props["plan_cost_over_time"] = [float(s) for s in matches]
 
 def main():
     parser = Parser()    
